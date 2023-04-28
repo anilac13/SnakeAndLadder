a93a26ba337399f933ad307291b919e59e3c0964
anilac13 <anil4u2496@gmail.com>
2023-04-28 16:44:38 +0530

    [Anil] Add:Player check for option No play,Ladder,Snake

--- SnakeAndLadder/PlayGame.cs
+++ SnakeAndLadder/PlayGame.cs
@@ -11,9 +11,25 @@ namespace SnakeAndLadder
         public static void Play()
         {
             int playerPosition = 0;
+            const int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
             Random random = new Random();
             int rollDie = random.Next(1, 7);
             Console.WriteLine("Die number is: " + rollDie);
+            int option = random.Next(3);
+            switch (option)
+            {
+                case NO_PLAY:
+                    playerPosition = 0;
+                    break;
+                case LADDER:
+                    playerPosition += rollDie;
+                    Console.WriteLine("Player position ===> " + playerPosition);
+                    break;
+                case SNAKE:
+                    playerPosition -= rollDie;
+                    Console.WriteLine("Player position ===> " + playerPosition);
+                    break;
+            }
         }
     }
 }
